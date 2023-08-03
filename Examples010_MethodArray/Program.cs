52d993a03f79e49c00a1d61f6dc157f476ae7871
MihailAulov <titul7777@mail.ru>
2023-08-03 20:31:11 +0300

    Коммит 10

---
+++ Examples010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {1, 4, 33, 7, 57, 66, 74, 86};
+
+int n = array.Length;
+int find = 5;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+
+    index++;
+}
