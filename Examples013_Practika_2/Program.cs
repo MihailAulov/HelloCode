e6754707fa8a93ff71451779c64d8538d304f0e9
MihailAulov <titul7777@mail.ru>
2023-08-07 22:11:09 +0300

    add Example013_Practika_2

---
+++ Examples013_Practika_2/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int newNumber = new Random().Next(100, 1000);
+Console.WriteLine($"Число : {newNumber}");
+Console.WriteLine((newNumber / 100) * 10 + (newNumber % 10));
