71f4c807ecfb388ba6e510f47161827b8e4eec2d
MihailAulov <titul7777@mail.ru>
2023-09-03 00:05:17 +0300

    ДЗ сделал

---
+++ HomeWork_C#_1_task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+
+int a = new Random().Next(0, 1000); 
+int b = new Random().Next(0, 1000);
+
+Console.Write("a = ");
+Console.Write(a);
+
+if (a > b)
+{
+    Console.Write(" больше ");
+}
+else
+{
+    Console.Write(" меньше ");
+};
+
+Console.Write("b = ");
+Console.Write(b);
---
+++ HomeWork_C#_1_task_6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+﻿Console.Write("Введите число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number2 % 2 == 0)
+{
+    Console.WriteLine($"число {number2} чётное");  
+}
+else
+{
+    Console.WriteLine($"число {number2} нечётное");  
+};
