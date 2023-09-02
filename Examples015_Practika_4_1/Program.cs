2869f7268801374bb4ff97b0520f46206fd8ba35
MihailAulov <titul7777@mail.ru>
2023-09-02 22:01:19 +0300

    Задача 27 завершил

--- Examples015_Practika_4_1/Program.cs
+++ Examples015_Practika_4_1/Program.cs
@@ -1,2 +1,16 @@
 ﻿//Напишите программу которая принимает на вход число и выдает количество цифр в числе.
 
+Console.WriteLine(GetLenghtNum(48736));
+
+int GetLenghtNum(int num)
+{
+    int len = default; // так же = 0;
+// System.Console.WriteLine(len); // проверили число присвоения
+    while(num > 0)
+    {
+        len++;
+        num /= 10;
+    }
+    
+    return len;
+}
---
+++ Examples015_Practika_4_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу которая выводит массив из 8 элементов, заполненных нулями в случайном порядке.
+
+PrintArray(GenerateArray(10));
+
+int[] GenerateArray(int lenght)
+{
+    int[] result = new int[lenght];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(0, 2);
+    }
+
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int i in array) // перебирает непосредственно значение элементов мвссива
+    {
+        Console.Write($"{i} ");
+    }
+}
