26d1b3a68a1544d7fca1265282168ce89a395ee3
MihailAulov <titul7777@mail.ru>
2023-09-02 20:37:47 +0300

    Начали практиику

--- Examples015_Practika_4/Program.cs
+++ Examples015_Practika_4/Program.cs
@@ -1,2 +1,15 @@
 ﻿// НАпишите программу, которая принимает на вход число (A) и выдаёт сумму чисел от 1 до A.
 
+int GetSumFrom1ToN(int a)
+{
+    int result = 0;
+    int count = 0;
+
+    while (count <= a)
+    {
+       result += count;
+       count ++;
+    }
+    
+    return result;
+}
