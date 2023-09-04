e1900b989a919d5ebc8b66a4af4fb6b45ef38eab
MihailAulov <titul7777@mail.ru>
2023-09-04 22:47:16 +0300

    Работает

---
+++ Examples015_Practika_6/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Напишите программу,
+которая перевернёт одномерный массив
+(последний элемент будет на первом месте, а первый на последнем и т.д.) */
+
+var arr = GetRandomArray(10);
+PrintArray(arr);
+ReverseArray(arr);
+System.Console.WriteLine();
+PrintArray(arr);
+
+void ReverseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int tmp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = tmp;
+    }
+}
+
+int[] GetRandomArray(int lenght)
+{
+    int[] result = new int[lenght];
+
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(1, 100);
+    }
+
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array) 
+    {
+        Console.Write(item + "  ");
+    }
+
+    Console.WriteLine();
+}
---
+++ Examples015_Practika_6_1/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите прогармму,
+которая принимает на вход три числа
+и проверяет, может ли существоавать
+треугольник со сторонами такой длины. */
+
+
+System.Console.WriteLine(IsTriangle(1, 1, 9));
+
+bool IsTriangle(int stA, int stB, int stC)
+{
+    return (((stA + stB) > stC) && ((stB + stC) > stA) && ((stC + stA) > stB));
+}
+
+// Второе решение - лямда выражение
+
+/* var isTriangle = (int stA, int stB, int stC) =>
+     (((stA + stB) > stC) && ((stB + stC) > stA) && ((stC + stA) > stB));
+    
+System.Console.WriteLine(isTriangle(1, 1, 9)); */ 
