5100a75806df0897cd09213ea700ebc420e3b8be
MihailAulov <titul7777@mail.ru>
2023-09-04 23:01:33 +0300

    Работает

--- Examples015_Practika_6_1/Program.cs
+++ Examples015_Practika_6_1/Program.cs
@@ -3,13 +3,13 @@
 и проверяет, может ли существоавать
 треугольник со сторонами такой длины. */
 
-
-System.Console.WriteLine(IsTriangle(1, 1, 9));
+// Решение первое
+/* System.Console.WriteLine(IsTriangle(1, 1, 9));
 
 bool IsTriangle(int stA, int stB, int stC)
 {
     return (((stA + stB) > stC) && ((stB + stC) > stA) && ((stC + stA) > stB));
-}
+} */
 
 // Второе решение - лямда выражение
 
@@ -17,3 +17,25 @@ bool IsTriangle(int stA, int stB, int stC)
      (((stA + stB) > stC) && ((stB + stC) > stA) && ((stC + stA) > stB));
     
 System.Console.WriteLine(isTriangle(1, 1, 9)); */ 
+
+
+// Углубленое изучение
+
+var persons = new List <Person> {
+    new Person { 
+        Id = 1, 
+        Name = "Dima"
+    },
+    new Person { 
+        Id = 2, 
+        Name = "Misha"
+    },
+};
+
+System.Console.WriteLine(persons.Where(p => p.Id == 2).FirstOrDefault().Name);
+
+public class Person
+{
+    public int Id { get; set;}
+    public string Name { get; set;}
+}
